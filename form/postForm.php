6b1fd36ad3524ecaf2fd86955497893d4a29377a
Alejandro Mc Ewen <mcewenalejandro@gmail.com>
2023-01-31 10:01:25 -0500

    First homework

---
+++ form/postForm.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "Hero1234";
+$dbname = "TEST_PHP";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sqlquery = "INSERT INTO TEST_NAME VALUES
+    ('" . $_REQUEST["name"] . "')";
+
+if (mysqli_query($conn, $sqlquery) === TRUE) {
+    echo "<h3>data stored in a database successfully."
+    . " Please browse your localhost php my admin"
+    . " to view the updated data</h3>";
+} else {
+    echo "Error: " . $sqlquery . "<br>" . $conn->error;
+}
+mysqli_close($conn);
+?>
